b62c453e0be4f77daa1301b4ec54f9e67943c76d
Vincent Besseau <vbesseau44@gmail.com>
2018-09-22 01:27:36 +0200

    Adding exec method on module

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
 module.exports = function(sails) {
 	
-	const setup = require('./lib/viera.setup');
-	const init = require('./lib/viera.init');
+	const setup = require('./lib/viera.setup.js');
+	const init = require('./lib/viera.init.js');
+	const exec = require('./lib/viera.exec.js');
 	const television = require('./lib/viera.js');
 
 	gladys.on('ready', function(){
-		init()
+		init();
 	});
  
     return {
 		setup: setup,
+		exec: exec,
 		television: television
     };
 };
