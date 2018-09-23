afc52a3b428f8baa50c8073b1f47303b4fc81970
Vincent Besseau <vbesseau44@gmail.com>
2018-09-23 17:53:13 +0200

    Adding voice controle on viera module

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ module.exports = function(sails) {
 	const setup = require('./lib/viera.setup.js');
 	const init = require('./lib/viera.init.js');
 	const exec = require('./lib/viera.exec.js');
+	var command = require('./lib/viera.command.js');
 	const television = require('./lib/viera.js');
+	const install = require('./lib/viera.install.js');
+	const uninstall = require('./lib/viera.uninstall.js');
 
 	gladys.on('ready', function(){
 		init();
 	});
  
     return {
-		setup: setup,
-		exec: exec,
-		television: television
+			setup: setup,
+			exec: exec,
+			command: command,
+			install: install,
+			television: television,
+			uninstall: uninstall
     };
 };
